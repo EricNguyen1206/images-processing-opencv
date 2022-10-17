e1845a46771511b3faa0e07f55ebdfc5759810ff
ftwanlin <ftwanlin@gmail.com>
2022-10-17 20:44:08 +0700

    them face detection

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
+
 def tinh_hist(img):
     hist = np.zeros((256,), np.uint8)  # tạo mảng để thống kê số lượng pixel cho từng mức sáng
     h, w = img.shape[:2]  # lấy số đo của ảnh
@@ -18,6 +19,24 @@ def can_bang_hist(hist):
     new_hist = (trans - trans.min()) / (trans.max() - trans.min()) * 255
     new_hist = np.uint8(new_hist)
     return new_hist
+
+
+def face_detect(img):
+    # load cascade
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+    # chuyển thành ảnh xám
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # nhận diện
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+    # vẽ hình vuông nhận diện
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    return img
+
+
 
 
 img = cv2.imread("d.png")
